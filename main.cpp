34850392342dd10fa1dfee2426b260002e2a917e
Hesam <amirhesam.chehreghani@gmail.com>
2026-02-05 16:39:37 +0330

    including SDL headers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL2_gfx.h>
+
 using namespace std;
+
 int main() {
     
     return 0;
